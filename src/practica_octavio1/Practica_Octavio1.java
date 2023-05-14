78ef07d61a1de2979954aa90480c5db21c569909
karliwi12 <126117711+karliwi12@users.noreply.github.com>
2023-05-14 10:18:05 -0500

    Initial commit

---
+++ src/practica_octavio1/Practica_Octavio1.java
@@ -0,0 +1,16 @@
+
+package practica_octavio1;
+
+import practica_octavio1.Formulario.Principal;
+
+
+public class Practica_Octavio1 {
+
+   
+    public static void main(String[] args) {
+        Principal p = new Principal();
+        p.setVisible(true);
+        
+    }
+    
+}
